74366a0123679292867a63e2223ca0e6ac153bda
hammadbuic <hammadahmadbuic@gmail.com>
2021-10-18 12:57:34 +0500

    EmailService

---
+++ EmailSender.cs
@@ -0,0 +1,79 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using MailKit.Net.Smtp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public class EmailSender:IEmailSender
+    {
+        private readonly EmailConfiguration _emailConfiguration;
+        public EmailSender(EmailConfiguration emailConfiguration)
+        {
+            _emailConfiguration = emailConfiguration;
+        }
+        public void SendEmail(Message msg)
+        {
+            var emailMsg = CreateEmailMessage(msg);
+            Send(emailMsg);
+        }
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_emailConfiguration.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+            return emailMessage;
+        }
+        private void Send(MimeMessage mailMessage)
+        {
+            using(var client=new SmtpClient())
+            {
+                try 
+                {
+                    client.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_emailConfiguration.UserName, _emailConfiguration.Password);
+                } catch { throw; } 
+                finally 
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+        public async Task SendEmailAsync(Message message)
+        {
+            var mailMessage = CreateEmailMessage(message);
+
+            await SendAsync(mailMessage);
+        }
+        private async Task SendAsync(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(_emailConfiguration.SmtpServer, _emailConfiguration.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    await client.AuthenticateAsync(_emailConfiguration.UserName, _emailConfiguration.Password);
+
+                    await client.SendAsync(mailMessage);
+                }
+                catch
+                {
+                    //log an error message or throw an exception, or both.
+                    throw;
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
